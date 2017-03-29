e5eadd14155c45dc7b9bbc7df68c64b321b2e50c
shincling <shijing609@163.com>
2017-03-29 19:23:56 +0800

    My first cpp code.

---
+++ first_CPP/first_CPP/test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+const double First=1.0;
+const double Twice=2.0;
+
+int main()
+{
+	int Trible=3;
+	double var_1;
+	double var_2;
+	double var_3;
+	cout << "Enter number multiple 1: =>";
+	cin >> var_1;
+	cout << "Enter number plus 2: =>";
+	cin >> var_2;
+	cout << "Enter number divide 3: =>";
+	cin >> var_3;
+}
+
+	

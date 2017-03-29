2710e23801b56645b4e1cd4d91f3a2cea6772b2d
shincling <shijing609@163.com>
2017-03-29 21:36:29 +0800

    To add the if_test in its own project , but in the whole solution

---
+++ first_CPP/if_Pro/if_test_in_Pro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double aa;
+	cout <<"Enter a number:";
+	cin >> aa;
+	if (aa>2)
+		cout <<"This number is over 2!"<<endl;
+	else
+		cout <<"This number is less or equal 2"<<endl;
+	cin >>aa;
+	return 0;
+}

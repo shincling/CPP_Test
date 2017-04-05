d11c3c02c1b91f2091c4f3596898c73204944aa2
shincling <shijing609@163.com>
2017-04-05 16:16:56 +0800

    Add the pause .

--- first_CPP/if_Pro/if_test_in_Pro.cpp
+++ first_CPP/if_Pro/if_test_in_Pro.cpp
@@ -10,6 +10,7 @@ int main()
 		cout <<"This number is over 2!"<<endl;
 	else
 		cout <<"This number is less or equal 2"<<endl;
-	cin >>aa;
+	//cin >>aa;
+	system("pause");//暂停，使得窗口不直接退出
 	return 0;
 }

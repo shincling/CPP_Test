6560904c91c8fe32a7eba00100087d762b726364
shincling <shijing609@163.com>
2017-04-05 19:30:34 +0800

    More test about math and cmath

--- first_CPP/math_expression_Pro/math_expression.cpp
+++ first_CPP/math_expression_Pro/math_expression.cpp
@@ -1,17 +1,28 @@
 #include<string>
 #include<iostream>
+//#include<math.h>
+#include<cmath>
+
 using namespace std;
 
 int main()
 {
 	cout<<4*.3<<endl;
 	cout<<4/3<<endl;
-	cout<<0.1+4/3<<endl;//注意这里，还是有问题的
-	
+	cout<<double(4)/3<<endl;
+	cout<<0.1+4/3<<endl;
 	double q=9/2;//notice this will cause error,answer with 4.0
 	cout<<q<<endl;
 	cout<<3.00000<<endl;//the cout will show the 3.0 as 3
+	
+	int x=3.99999;
+	cout<<x<<"\n"<<endl;//direct omit the xiaoshu part
 
+	
+	cout<<sqrt(3.0)<<endl;
+	cout<<sqrt(4.0)<<endl;
+	cout<<fabs(-4.0)<<endl;
+	
 	system("pause");
 	return 0;
 }

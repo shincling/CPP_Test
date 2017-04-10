b7c37d3ac24a1ac224f32a80dadd0d5ae572bfa6
shincling <shijing609@163.com>
2017-04-10 20:11:31 +0800

    if 多个语句的时候 是要用大括号的，和python的缩进控制不同

--- first_CPP/if_Pro/if_test_in_Pro.cpp
+++ first_CPP/if_Pro/if_test_in_Pro.cpp
@@ -11,6 +11,15 @@ int main()
 	else
 		cout <<"This number is less or equal 2"<<endl;
 	//cin >>aa;
+	if (aa<0)
+		cout <<"Negtive"<<endl;
+	else if (aa<5)
+	{
+		cout <<"Positive"<<endl;
+		cout <<"Positive again"<<endl;
+	}
+	else
+		cout <<"Positive and more than 5"<<endl;
 	system("pause");//暂停，使得窗口不直接退出
 	return 0;
 }
